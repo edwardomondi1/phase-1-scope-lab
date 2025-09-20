fc288c5b46b461da99fc386efd17298bfa93635f
edwardomondi1 <edward.omondi1@student.moringaschool.com>
2025-09-20 09:39:55 +0300

    message

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Global variable customerName
+var customerName = 'bob';
+
+// Function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite the bestCustomer variable
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+// Global constant leastFavoriteCustomer
+const leastFavoriteCustomer = 'bob';
+
+// Function that attempts to change the constant (will throw an error)
+function changeLeastFavoriteCustomer() {
+  try {
+    leastFavoriteCustomer = 'someone else';
+  } catch (error) {
+    throw new Error('Assignment to constant variable.');
+  }
+}
